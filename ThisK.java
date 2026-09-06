fff3b95e22e9520b0b674b76c583ec562e286f63
^Deepak-ok <deepak.shishupal.tomar2003 git config --global user.email>
2026-09-06 23:49:24 +0530

    8

---
+++ ThisK.java
@@ -0,0 +1,37 @@
+class Example{
+     int x;
+     int y;
+    void f1(int x,int y){
+        this.x=x;  //this keyword refers to the current 
+        this.y=y;
+    }
+    void display()
+    {
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
+class Example1 extends Example{
+    int x,y;
+    void f3(int x,int y){
+        super.x=x; //super keyword is refers to the parent class
+        super.y=y;
+    }
+    void f4()
+    {
+        System.out.println(super.x);
+        System.out.println(this.y);
+    }
+}
+class ThisK {
+    public static void main(String[]args)
+    {
+        Example1 e1=new Example1();
+        e1.f3(2,3);
+        e1.f4();
+        // Example e1=new Example();
+        // e1.f1(2,3);
+        // e1.display();
+    }
+    
+}
