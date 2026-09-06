896601a9737a61abae3f4c006198f733ca27edb6
^Deepak-ok <deepak.shishupal.tomar2003 git config --global user.email>
2026-09-06 15:13:12 +0530

    ok

---
+++ StaticMember.java
@@ -0,0 +1,29 @@
+class A{
+   private int x; 
+    private static int y;
+    void fun1()
+    {
+        y=22;
+        x=5;
+    }
+    void fun2()
+    {
+        System.out.println(x);
+    }
+    void fun3()
+    {
+        y=15;
+
+    }
+}
+class StaticMember{
+    public static  void main(String[]args)
+    {
+        A a1=new A();
+         A a2=new A();
+            a1.fun1();
+          //  a2.fun3();
+           // a1.fun2();
+            a2.fun2();
+    }
+}
