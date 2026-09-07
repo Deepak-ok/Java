ef91041068dba260f6c132f020fe589cbb56fd96
^Deepak-ok <deepak.shishupal.tomar2003 git config --global user.email>
2026-09-07 10:51:36 +0530

    10

---
+++ Interface.java
@@ -0,0 +1,54 @@
+//interface fuction has already predefined that is already(public,static,abstract or final). So that's why it need to override its function for make it usable
+//interface can't have a constructor
+//can't make object of a interface
+//but if made afunction static then you make a body
+interface i1 {
+
+    int x = 5;
+
+    void f1();
+
+    static void f2() {
+        System.out.println(x);
+    }
+}
+
+interface i2 extends i1 {
+
+    int x = 9;
+    int y = 2;
+
+    static void f2() {
+        System.out.println(x);
+    }
+
+    void f3();
+
+}
+
+class c1 implements i2 {
+
+    static void f2() {
+        System.out.println(x);
+    }
+
+    public void f1() {
+
+    }
+
+    public void f3() {
+
+    }
+}
+
+class Interface {
+
+    public static void main(String[] args) {
+        i2.f2();  // static variable can be inherited
+        //i2.f2(); static member function cannot be inherited
+        // c1 C1=new c1();
+        // C1.f2();
+        //System.out.println(C1.y);
+        // i1.f2();
+    }
+}
